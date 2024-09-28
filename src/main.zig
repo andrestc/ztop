e5c21732bfdc592d86f61d7e278a01c7d889ae4f
Andre Carvalho <asantostc@gmail.com>
2024-09-28 21:27:01 +0100

    checkpoint

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,21 @@
 const std = @import("std");
 
 const Process = struct {
+    allocator: std.mem.Allocator,
     pid: u32,
     command: []const u8,
+
+    pub fn init(allocator: std.mem.Allocator, pid: u32, command: []const u8) !Process {
+        return .{
+            .allocator = allocator,
+            .pid = pid,
+            .command = command,
+        };
+    }
+
+    pub fn deinit(self: Process) void {
+        self.allocator.free(self.command);
+    }
 };
 
 const ProcessList = struct {
@@ -17,53 +30,47 @@ const ProcessList = struct {
     }
 
     pub fn deinit(self: ProcessList) void {
-        defer self.list.deinit();
         for (self.list.items) |p| {
-            self.allocator.free(p.command);
+            p.deinit();
         }
+        self.list.deinit();
     }
 };
 
 const ProcReader = struct {
     allocator: std.mem.Allocator,
     proc_path: []const u8,
-    proc_dir: *std.fs.Dir,
 
     pub fn init(allocator: std.mem.Allocator, proc_path: []const u8) !ProcReader {
-        var proc_dir = try std.fs.openDirAbsolute(proc_path, std.fs.Dir.OpenDirOptions{ .iterate = true });
         return ProcReader{
             .proc_path = proc_path,
             .allocator = allocator,
-            .proc_dir = &proc_dir,
         };
     }
 
-    pub fn deinit(self: ProcReader) void {
-        self.proc_dir.close();
-    }
-
-    fn readCmdLine(self: ProcReader, pid: u32, buffer: []u8) !usize {
-        const path = try std.fmt.allocPrint(self.allocator, "{s}/{d}/cmdline", .{ self.proc_path, pid });
+    fn readCmdLine(self: ProcReader, proc_dir: std.fs.Dir, pid: u32, buffer: []u8) !usize {
+        const path = try std.fmt.allocPrint(self.allocator, "{d}/cmdline", .{pid});
         defer self.allocator.free(path);
 
-        var cmdline = try std.fs.openFileAbsolute(path, std.fs.File.OpenFlags{});
-        return try cmdline.readAll(buffer);
-    }
-
-    fn readComm(self: ProcReader, pid: u32, buffer: []u8) !usize {
-        const path = try std.fmt.allocPrint(self.allocator, "{s}/{d}/comm", .{ self.proc_path, pid });
-        defer self.allocator.free(path);
-
-        var cmdline = try std.fs.openFileAbsolute(path, std.fs.File.OpenFlags{});
+        var cmdline = try proc_dir.openFile(path, .{});
 
         return try cmdline.readAll(buffer);
     }
 
-    fn readCommand(self: ProcReader, pid: u32) ![]u8 {
+    fn readComm(self: ProcReader, proc_dir: std.fs.Dir, pid: u32, buffer: []u8) !usize {
+        const path = try std.fmt.allocPrint(self.allocator, "{d}/comm", .{pid});
+        defer self.allocator.free(path);
+
+        var cmdline = try proc_dir.openFile(path, .{});
+
+        return try cmdline.readAll(buffer);
+    }
+
+    fn readCommand(self: ProcReader, proc_dir: std.fs.Dir, pid: u32) ![]u8 {
         var buffer: [256]u8 = undefined;
-        var read = try self.readCmdLine(pid, &buffer);
+        var read = try self.readCmdLine(proc_dir, pid, &buffer);
         if (read == 0) {
-            read = try self.readComm(pid, &buffer);
+            read = try self.readComm(proc_dir, pid, &buffer);
             read = read - 1;
         }
         if (read <= 0) {
@@ -73,15 +80,20 @@ const ProcReader = struct {
     }
 
     pub fn readProcesses(self: ProcReader) !ProcessList {
-        var iter = self.proc_dir.iterate();
+        var proc_dir = try std.fs.openDirAbsolute(self.proc_path, std.fs.Dir.OpenDirOptions{ .iterate = true });
+        defer proc_dir.close();
+
+        var iter = proc_dir.iterate();
         var process_list = try ProcessList.init(self.allocator);
+        errdefer process_list.deinit();
+
         while (try iter.next()) |entry| {
             if (entry.kind != std.fs.File.Kind.directory) {
                 continue;
             }
             const pid: u32 = std.fmt.parseInt(u32, entry.name, 10) catch continue;
-            const exec = self.readCommand(pid) catch "Unknown";
-            try process_list.list.append(Process{ .pid = pid, .command = exec });
+            const exec = self.readCommand(proc_dir, pid) catch "Unknown";
+            try process_list.list.append(try Process.init(self.allocator, pid, exec));
         }
         return process_list;
     }
@@ -93,7 +105,6 @@ pub fn main() !void {
     const alloc = gpa.allocator();
 
     const proc_reader = try ProcReader.init(alloc, "/proc/");
-    defer proc_reader.deinit();
 
     const process_list = try proc_reader.readProcesses();
     defer process_list.deinit();
