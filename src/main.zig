d33ec38a4b5203ad1f948c177997803632538445
Andre Carvalho <asantostc@gmail.com>
2024-09-28 23:07:57 +0100

    parse /proc/<pid>/status

--- src/main.zig
+++ src/main.zig
@@ -5,17 +5,45 @@ const Process = struct {
     allocator: std.mem.Allocator,
     pid: u32,
     command: []const u8,
+    status: Status,
 
-    pub fn init(allocator: std.mem.Allocator, pid: u32, command: []const u8) !Process {
+    const Status = struct {
+        allocator: std.mem.Allocator,
+        state: []const u8,
+
+        pub fn init(allocator: std.mem.Allocator, file: std.fs.File) !Status {
+            var buf_reader = std.io.bufferedReader(file.reader());
+            var in_stream = buf_reader.reader();
+            var buf: [1024]u8 = undefined;
+            var state: []u8 = undefined;
+            while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+                var it = std.mem.split(u8, line, ":\t");
+
+                if (std.mem.eql(u8, it.first(), "State")) {
+                    state = try std.mem.Allocator.dupe(allocator, u8, it.next().?);
+                }
+            }
+
+            return .{
+                .allocator = allocator,
+                .state = state,
+            };
+        }
+    };
+
+    pub fn init(allocator: std.mem.Allocator, pid: u32, command: []const u8, status: Status) !Process {
         return .{
             .allocator = allocator,
             .pid = pid,
             .command = command,
+            .status = status,
         };
     }
 
     pub fn deinit(self: Process) void {
         self.allocator.free(self.command);
+        // TODO: move this to Process.Status
+        self.allocator.free(self.status.state);
     }
 };
 
@@ -54,6 +82,7 @@ const ProcReader = struct {
         defer self.allocator.free(path);
 
         var cmdline = try proc_dir.openFile(path, .{});
+        defer cmdline.close();
 
         return try cmdline.readAll(buffer);
     }
@@ -63,6 +92,7 @@ const ProcReader = struct {
         defer self.allocator.free(path);
 
         var cmdline = try proc_dir.openFile(path, .{});
+        defer cmdline.close();
 
         return try cmdline.readAll(buffer);
     }
@@ -80,6 +110,15 @@ const ProcReader = struct {
         return try std.mem.Allocator.dupe(self.allocator, u8, buffer[0..read]);
     }
 
+    fn readStatus(self: ProcReader, proc_dir: fs.Dir, pid: u32) !Process.Status {
+        const path = try std.fmt.allocPrint(self.allocator, "{d}/status", .{pid});
+        defer self.allocator.free(path);
+
+        var status_file = try proc_dir.openFile(path, .{});
+        defer status_file.close();
+        return try Process.Status.init(self.allocator, status_file);
+    }
+
     pub fn readProcesses(self: ProcReader) !ProcessList {
         var proc_dir = try fs.openDirAbsolute(self.proc_path, std.fs.Dir.OpenDirOptions{ .iterate = true });
         defer proc_dir.close();
@@ -94,7 +133,8 @@ const ProcReader = struct {
             }
             const pid: u32 = std.fmt.parseInt(u32, entry.name, 10) catch continue;
             const exec = self.readCommand(proc_dir, pid) catch "Unknown";
-            try process_list.list.append(try Process.init(self.allocator, pid, exec));
+            const status = try self.readStatus(proc_dir, pid);
+            try process_list.list.append(try Process.init(self.allocator, pid, exec, status));
         }
         return process_list;
     }
@@ -110,7 +150,8 @@ pub fn main() !void {
     const process_list = try proc_reader.readProcesses();
     defer process_list.deinit();
 
+    std.debug.print("PID\tSTATE\tCMD\n", .{});
     for (process_list.list.items) |p| {
-        std.debug.print("{d}\t{s}\n", .{ p.pid, p.command });
+        std.debug.print("{d}\t{s}\t{s}\n", .{ p.pid, p.status.state, p.command });
     }
 }
